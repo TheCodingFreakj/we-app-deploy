19222eb14491d380fcb9e361703ac0bf7f85bb65
pallavidapriya <pallavidapriya75@gmail.com>
2021-05-18 08:44:48 +0530

    commit for aws

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Set our backend port to be either an environment variable or port 5000
 const port = process.env.PORT || 5050;
-
+console.log(process.env.PORT);
 // This application level middleware prints incoming requests to the servers console, useful to see incoming requests
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -36,12 +36,15 @@ app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
   mongoose
-    .connect(process.env.mongo_url_prod, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    })
+    .connect(
+      "mongodb+srv://pallavi57:pallavi57@cluster0.07c2o.mongodb.net/mernstackdb?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+      }
+    )
     .then(() => console.log("DB Connected!"))
     .catch((err) => {
       console.log(`DB Connection Error: ${err.message}`);
@@ -51,17 +54,26 @@ if (process.env.NODE_ENV === "production") {
   process.env.NODE_ENV === "staging"
 ) {
   mongoose
-    .connect(process.env.mongo_url_dev, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    })
+    .connect(
+      "mongodb+srv://pallavi57:pallavi57@cluster0.07c2o.mongodb.net/mernstackdb?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+      }
+    )
     .then(() => console.log(`Database connected to developement db`))
     .catch((err) => {
       console.log(`DB Connection Error: ${err.message}`);
     });
 }
+
+//set static folder
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 // Require Route
 const api = require("./routes/route");
@@ -70,18 +82,5 @@ const UserRoute = require("./routes/userRoute");
 app.use("/api/v1", UserRoute);
 const ProductRoute = require("./routes/productRoute");
 app.use("/api/v1", ProductRoute);
-// This middleware informs the express application to serve our compiled React files
 
-if (process.env.NODE_ENV === "production") {
-  //set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
-// Configure our server to listen on the port defiend by our port variable
 app.listen(port, () => console.log(`BACK_END_SERVICE_PORT: ${port}`));
-// NOTE: You may need to modify "server": "nodemon server.js",
-// depending on where your sever.js is located and the name you have given.
-//  In this case, server.js is in the same level as package.json.
